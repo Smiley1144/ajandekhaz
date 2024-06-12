4cc6f6f5a3ac3eda82f811088d0ad88a9556d702
Smiley1144 <kovacsm519@gmail.com>
2024-06-12 15:21:30 +0200

    getProduct functions

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,12 +1,43 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Firestore, collection, doc, getDoc, getDocs } from '@angular/fire/firestore';
+import { Observable, from, map } from 'rxjs';
 import { ProductModel } from 'src/app/models/product.model';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  
+  private readonly productCollectionRef = collection(this.firestore, 'products')
+ 
+  constructor(private firestore: Firestore) {
+    
+  }
+//* Get all products
+  getProductsWithGetDocs(): Observable<ProductModel[]> {
+    return from(getDocs(this.productCollectionRef)).pipe(
+      map((snapshot) => {
+        const resultList = snapshot.docs.map((doc) => {
+          const productData: ProductModel = doc.data() as ProductModel;
+          productData.id = doc.id;
+          return productData;
+        });
+        return resultList;
+      })
+    );
+  }
+
+  //* Get only one product
+  getProductWithGetDoc(id: string){
+    const productDoc = doc(this.firestore, `product/${id}`);
+    return from(getDoc(productDoc)).pipe(
+      map((doc) => {
+        const productData: ProductModel = doc.data() as ProductModel;
+        productData.id = doc.id;
+        return productData;
+      })
+    )
+  }
 }
 
