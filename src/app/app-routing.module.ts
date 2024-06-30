76af26d6a7c2068b70a40937c629080bf7cb14a0
Varga Tamás <scorpagon@gmail.com>
2024-06-30 12:56:27 +0200

    navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProductsComponent } from './modules/products/products.component';
 import { CartComponent } from './modules/cart/cart.component';
 import { WelcomePageComponent } from './modules/welcome-page/welcome-page.component';
+import { NavBarComponent } from './modules/nav-bar/nav-bar.component';
 
 const routes: Routes = [
-  {path: '', component: WelcomePageComponent},
-  {path: 'products', component: ProductsComponent},
-  {path: 'cart', component: CartComponent}
+  { path: '', component: WelcomePageComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'navBar', component: NavBarComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/modules/nav-bar/nav-bar.component.ts
+++ src/app/modules/nav-bar/nav-bar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
+  navbarOpen = false;
 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
