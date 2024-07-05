87023df75e3922c311e4c175f97288ac0a5f82c5
Varga Tamás <scorpagon@gmail.com>
2024-07-05 16:44:18 +0200

    Cart - hibákkal

--- src/app/modules/nav-bar/nav-bar.component.ts
+++ src/app/modules/nav-bar/nav-bar.component.ts
@@ -1,15 +1,25 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
   navbarOpen = false;
 
-  toggleNavbar() {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  toggleNavbar(): void {
     this.navbarOpen = !this.navbarOpen;
   }
 }
