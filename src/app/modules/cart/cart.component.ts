9eb0d76c527d96442812d3deb5e4164a995b5ff5
Smiley1144 <kovacsm519@gmail.com>
2024-07-05 17:52:42 +0200

    cart html interpolation fix

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { ProductModel } from 'src/app/models/product.model';
+import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss'],
 })
-export class CartComponent {}
+export class CartComponent {
+  productList: ProductModel[] = []
+  constructor(private productService: ProductService){}
+}
