2ead42ed1bb6d4fbd001eebb9f917b8940133601
Smiley1144 <kovacsm519@gmail.com>
2024-07-07 22:35:11 +0200

    remove pagination from prod.ts

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -32,10 +32,6 @@ export class ProductsComponent implements OnInit {
     this.midButton = (document.getElementById('mid') as HTMLButtonElement).value;
     this.highButton = (document.getElementById('high') as HTMLButtonElement).value;
 
-    this.products$ = of({ products: [], lastDoc: null });
-
-    this.loadProducts();
-
     this.subProduct = this.productService.getProductsWithGetDocs().subscribe({
       next: (product: ProductModel[]) => {
         this.productList = product;
@@ -52,41 +48,8 @@ export class ProductsComponent implements OnInit {
     
   }
 
-  loadProducts(lastDoc?: QueryDocumentSnapshot<any>) {
-    if (lastDoc) {
-      this.products$ = this.productService.getProductsWithPagination(lastDoc);
-    } else {
-      this.products$ = this.productService.getProductsWithPagination();
-    }
-
-    this.subProduct = this.products$.subscribe({
-      next: ({ products, lastDoc }) => {
-        this.productList = products;
-        this.filteredProducts = products;
-        this.lastDoc = lastDoc as QueryDocumentSnapshot<any>;
-        console.log('Products received in component: ', products);
-      },
-      error: (err) => {
-        console.log('Error: ', err);
-      },
-      complete: () => {
-        console.log('Product request is done!');
-      }
-    });
-  }
-
   img2Exists(product: any): boolean {
     return product.hasOwnProperty('img2');
-  }
-
-  nextPage() {
-    if (this.lastDoc) {
-      this.loadProducts(this.lastDoc);
-    }
-  }
-
-  previousPage() {
-    // Implement previous page logic if needed
   }
 
   goToProductDetails(index: number):void {
